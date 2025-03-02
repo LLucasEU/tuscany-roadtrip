0d83d2eebb7485df8ca054585e09dc9f85c03df9
Lucas <lucas.laisnard@epitech.eu>
2025-03-02 20:18:02 +0100

    Initial commit: Angular Tuscany Trip site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { ItineraryComponent } from './components/itinerary/itinerary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent,
+    ItineraryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
